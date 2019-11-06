0c7be997efdafbf8ebbde68d41a1927fcb08f7f6
dary <dary@ailleron.com>
2019-11-06 03:09:26 +0100

    fix container immediately exits

--- machine/src/test/java/com/baltic/machine/controller/MachineComputationControllerTest.java
+++ machine/src/test/java/com/baltic/machine/controller/MachineComputationControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,8 +14,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(MachineController.class)
-class MachineControllerTest {
+@WebMvcTest(MachineComputationController.class)
+class MachineComputationControllerTest {
     @Autowired
     private MockMvc mvc;
 
